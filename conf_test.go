12d4d79d44808cd0c5b631c3968530c08099f545
hezhaowu <hezhaowu@kingsoft.com>
2019-08-06 16:10:53 +0800

    save

--- conf_test.go
+++ conf_test.go
@@ -3,44 +3,28 @@ package conf
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
-func TestElem(t *testing.T) {
-	test, ok := Get("test")
-	if !ok {
-		fmt.Println("not existed")
-	}
-	fmt.Println(ElemBool(test, "bool"))
-	fmt.Println(ElemInt(test, "long"))
-	fmt.Println(ElemFloat64(test, "float"))
-	fmt.Println(ElemTime(test, "time"))
-	fmt.Println(ElemDuration(test, "duration"))
-	fmt.Println(ElemStringSlice(test, "slice"))
-	fmt.Println(ElemStringSlice(test, "slice2"))
-	fmt.Println(ElemStringMap(test, "map"))
-}
-
 func TestGet(t *testing.T) {
-	fmt.Println(GetBool("test.bool"))
-	fmt.Println(GetInt("test.long"))
-	fmt.Println(GetFloat64("test.float"))
-	fmt.Println(GetTime("test.time"))
-	fmt.Println(GetDuration("test.duration"))
-	fmt.Println(GetStringSlice("test.slice"))
-	fmt.Println(GetStringSlice("test.slice2"))
-	fmt.Println(GetStringMap("test.map"))
+	fmt.Println(Get(""))
+	fmt.Println(Get("pvp"))
+	fmt.Println(Get("pvp.kafkaNotifyLimit"))
 }
 
-func TestScan(t *testing.T) {
-	var m map[string]interface{}
-	start := time.Now()
-	for i := 0; i < 1000000; i++ {
-		Scan("test.map", &m)
-	}
-	end := time.Now()
-	fmt.Printf("used time: %v\n", end.Sub(start).Nanoseconds()/1000000)
-	fmt.Println(m)
+func TestGetBool(t *testing.T) {
+	fmt.Println(GetBool("pvp.testBool"))
+}
+
+func TestGetFloat64(t *testing.T) {
+	fmt.Println(GetFloat64("pvp.kafkaNotifyLimit"))
+}
+
+func TestGetDuration(t *testing.T) {
+	fmt.Println(GetDuration("pvp.redisNotityDelay"))
+}
+
+func TestGetTime(t *testing.T) {
+	fmt.Println(GetTime("pvp.beginDate"))
 }
 
 type Schedule struct {
